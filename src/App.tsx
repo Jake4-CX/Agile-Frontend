01a931d6752d9b1da8d385feb1df3f23388e716f
= <s4112037@glos.ac.uk>
2023-01-26 22:50:55 +0000

    Default

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+
+  return (
+    <body className="h-screen w-screen bg-slate-300">
+    <div className="flex items-center justify-center h-screen">
+      <div className="bg-white rounded-lg shadow-lg p-8 hover:bg-slate-100 duration-300">
+        <h1 className="font-bold cursor-pointer">React TypeScript Template</h1>
+      </div>
+    </div>
+    </body>
+  )
+}
+
+export default App
